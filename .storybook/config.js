21b5a8c63bc6e782e2476b1c542c78d2664feeee
NokKbl <asdfghjklnokky@gmail.com>
2019-06-13 00:43:51 +0700

    taskbox UI

---
+++ .storybook/config.js
@@ -0,0 +1,15 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+// import 'react-chromatic/storybook-addon';
+
+import '../src/index.css';
+
+// const req = require.context('../src', true, /.stories.js$/);
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  // require('../src/stories');
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
